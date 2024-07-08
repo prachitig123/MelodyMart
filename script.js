17e6f2c37dc0c25fd6de254365dfbed6447ab815
prachitig123 <123113029+prachitig123@users.noreply.github.com>
2024-07-09 00:58:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const header=document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY>80);
+});
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navlist');
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'85px',
+    duration:2500,
+    reset:true
+})
+sr.reveal('.home-text',{delay:300});
+sr.reveal('.home-img',{delay:400});
+sr.reveal('.about',{delay:300});
+sr.reveal('.about-img',{delay:400});
+sr.reveal('.review .middle-text',{delay:300});
+sr.reveal('.review-content',{delay:400});
+sr.reveal('.contact-text',{delay:300});
